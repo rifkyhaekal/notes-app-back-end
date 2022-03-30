9df3f9f3af2a9327a0bb0fc3e2475e00baa962ce
Haekal <177006076@student.unsil.ac.id>
2022-03-30 09:52:17 +0700

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthentiactionError = require('../../exceptions/AuthenticationsError');
 
 class UserService {
   constructor() {
@@ -61,6 +62,29 @@ class UserService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthentiactionError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthentiactionError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
   }
 }
 
